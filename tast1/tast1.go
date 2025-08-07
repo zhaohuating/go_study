4198dd905804821f28f49d6a78169ee4b6e6ec75
zhaoht <zhaoht@qq.com>
2025-08-07 19:37:13 +0800

    只出现一次的数字

--- tast1/tast1.go
+++ tast1/tast1.go
@@ -3,5 +3,26 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println(1111)
+	nums := []int{1, 2, 2, 4, 5, 7, 98, 5, 98}
+	onceNums := onceNum(nums)
+
+	fmt.Println(onceNums)
+}
+
+// 只出现一次的数字
+func onceNum(nums []int) []int {
+	numCount := make(map[int]int)
+	onceNums := make([]int, 0)
+
+	for _, num := range nums {
+		numCount[num]++
+	}
+
+	for num, count := range numCount {
+		if count == 1 {
+			onceNums = append(onceNums, num)
+		}
+	}
+
+	return onceNums
 }
